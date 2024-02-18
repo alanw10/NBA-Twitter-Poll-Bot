ee22cb3c9876916b94891c6fbc42e3437f9011ab
Alan Wang <53495995+alanw10@users.noreply.github.com>
2024-02-18 14:53:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import random, tweepy
+from nba_api.stats.static import players
+
+# gets the players names from the nba_api
+nba_players = players.get_players()
+
+
+active_players = [player for player in nba_players if player['is_active']]
+
+
+random_active_player = random.choice(active_players)
+random_active_player2 = random.choice(active_players)
+
+player1 = random_active_player['full_name']
+player2 = random_active_player2['full_name']
+
+
+# Enter API tokens below
+bearer_token = '' # not needed for this
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+#twitter authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+client = tweepy.Client(
+    bearer_token,
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret,
+    wait_on_rate_limit=True,
+)
+
+#tweeting part
+text = "Who is the better player?"
+poll_options = [player1,player2]
+# Send Tweet with Text and poll options
+client.create_tweet(text=text,poll_options = poll_options,poll_duration_minutes=1440) 
+print("Tweeted!")
